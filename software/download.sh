ca92fed88d2c4009eb8f908ef71c0dac9a6cdbc4
Ilmar Kerm <ilmar@ilmarkerm.eu>
2026-09-09 08:43:00 +0300

    updates

--- software/download.sh
+++ software/download.sh
@@ -3,21 +3,21 @@
 # Download software packages here
 
 # go
-declare -a gov="1.23.10 1.25.0 1.25.2"
+declare -a gov="1.25.14 1.26.8"
 for v in "${gov[@]}"; do
     fn = "go${v}.linux-arm64.tar.gz"
     [ -f "$fn" ] || curl -o "$fn" "https://go.dev/dl/${fn}"
 done
 
 # terraform
-vers="1.13.4"
+vers="1.16.1"
 fn="terraform_${vers}_linux_arm64.zip"
 [ -f "$fn" ] || curl -o "$fn" "https://releases.hashicorp.com/terraform/${vers}/${fn}"
 fn="terraform_${vers}_darwin_arm64.zip"
 [ -f "$fn" ] || curl -o "$fn" "https://releases.hashicorp.com/terraform/${vers}/${fn}"
 
 # vault
-vers="1.20.4"
+vers="1.21.4"
 fn="vault_${vers}_linux_arm64.zip"
 [ -f "$fn" ] || curl -o "$fn" "https://releases.hashicorp.com/vault/${vers}/${fn}"
 fn="vault_${vers}_darwin_arm64.zip"
